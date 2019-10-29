c63e6e5f487dcc01a0290294fefee2aff4b4b1db
leherv <viktor.leher@spl-tele.com>
2019-10-29 20:34:13 +0100

    added failing tests and corresponding Jenkinsfile

---
+++ FlaskServer/app/test_fail.py
@@ -0,0 +1,10 @@
+import unittest
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(3, 4)
+
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='/var/jenkins_home/workspace/spacerobots_2_master'))
+
